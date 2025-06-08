e513ed3f3bdea3db057dd50aafa4292590edcc94
Cory Buecker <email@corybuecker.com>
2025-06-08 07:23:58 -0500

    feat: add test server (#74)

--- client/src/collectors/page_view.ts
+++ client/src/collectors/page_view.ts
@@ -2,7 +2,6 @@ class PageView {
     constructor(endpoint: URL, appId: string) {
         document?.addEventListener("DOMContentLoaded", () => {
             const path = window?.location?.pathname;
-
             path && navigator.sendBeacon(endpoint.toString(), JSON.stringify({ entity: "page", action: "view", path, appId }))
         });
     }
